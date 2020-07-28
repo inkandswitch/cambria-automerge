1138c2397d6992caecefcd04bb48e940a0c34d01
Orion Henry <orion.henry@gmail.com>
2020-07-28 12:22:46 -0400

    test 3

--- test/cambriamerge.ts
+++ test/cambriamerge.ts
@@ -123,4 +123,50 @@ describe("Has basic schema tools", () => {
       type: 'map'
     })
   });
+
+  it("can accept a real change with its lens", () => {
+    const ProjectV1: LensSource = [
+      addProperty({ name: "title", type: "string" }),
+      addProperty({ name: "summary", type: "string" }),
+    ];
+
+    const doc1 = Backend.init({ schema: "projectv1" });
+
+    const [doc2, patch2] = Backend.applyChanges(doc1, [
+      {
+        kind: "lens",
+        from: "mu",
+        to: "projectv1",
+        lens: ProjectV1,
+      },
+      {
+        kind: "change" as const,
+        schema: "projectv1",
+        change: {
+          message: "",
+          actor: ACTOR_ID_1,
+          seq: 1,
+          deps: ['f758ca33017e3dc867dc10a8090ce0ff55ec461af8d9f45544a167d1ed74a3bf'],
+          time: 0,
+          startOp: 3, // applyLocalChange actually computes this for us
+          ops: [
+            {
+              action: "set",
+              obj: AUTOMERGE_ROOT_ID,
+              key: "title",
+              insert: false,
+              value: "hello",
+              pred: ["1@0000000000"]
+            },
+          ],
+        },
+      },
+    ]);
+
+    assert.deepEqual(patch2.diffs, {
+      objectId: '00000000-0000-0000-0000-000000000000',
+      props: { title: { '3@111111': { value: 'hello' } }, summary: { '2@0000000000': { value: '' } }  },
+      type: 'map'
+    })
+  });
 });
