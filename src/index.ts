d0367a9d0c80dabb894cf7e0ebabfda471d50fd4
Orion Henry <orion.henry@gmail.com>
2020-08-02 21:39:30 -0400

    fixing up exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export { init, applyChanges, applyLocalChange, getChanges, getPatch } from "./cambriamerge"
-export { RegisteredLens, CambriaBackend as BackDoc } from "./cambriamerge"
+export { RegisteredLens, CambriaBlock as Change, CambriaBackend as BackDoc } from "./cambriamerge"
 export { Frontend, Doc, ChangeFn, Proxy, Clock, Change as Request, Patch } from 'automerge'
+
+import { init, applyChanges, applyLocalChange, getChanges, getPatch } from "./cambriamerge"
+
+export const Backend = {
+  init,
+  applyChanges,
+  applyLocalChange,
+  getChanges,
+  getPatch
+}
