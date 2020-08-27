40c3fb8c43491298fc2c5e2d58cbb44ebf4f1b73
Geoffrey Litt <gklitt@gmail.com>
2020-08-27 15:35:18 -0400

    switch to prepare hook, require cambria deps properly

--- dist/index.js
+++ dist/index.js
@@ -1,34 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Backend = void 0;
-var cambriamerge_1 = require("./cambriamerge");
-Object.defineProperty(exports, "init", { enumerable: true, get: function () { return cambriamerge_1.init; } });
-Object.defineProperty(exports, "applyChanges", { enumerable: true, get: function () { return cambriamerge_1.applyChanges; } });
-Object.defineProperty(exports, "applyLocalChange", { enumerable: true, get: function () { return cambriamerge_1.applyLocalChange; } });
-Object.defineProperty(exports, "getChanges", { enumerable: true, get: function () { return cambriamerge_1.getChanges; } });
-Object.defineProperty(exports, "getPatch", { enumerable: true, get: function () { return cambriamerge_1.getPatch; } });
+const cambriamerge_1 = require("./cambriamerge");
 var cambriamerge_2 = require("./cambriamerge");
-Object.defineProperty(exports, "BackendState", { enumerable: true, get: function () { return cambriamerge_2.CambriaBackend; } });
+Object.defineProperty(exports, "init", { enumerable: true, get: function () { return cambriamerge_2.init; } });
+Object.defineProperty(exports, "applyChanges", { enumerable: true, get: function () { return cambriamerge_2.applyChanges; } });
+Object.defineProperty(exports, "applyLocalChange", { enumerable: true, get: function () { return cambriamerge_2.applyLocalChange; } });
+Object.defineProperty(exports, "getChanges", { enumerable: true, get: function () { return cambriamerge_2.getChanges; } });
+Object.defineProperty(exports, "getPatch", { enumerable: true, get: function () { return cambriamerge_2.getPatch; } });
+var cambriamerge_3 = require("./cambriamerge");
+Object.defineProperty(exports, "BackendState", { enumerable: true, get: function () { return cambriamerge_3.CambriaBackend; } });
 var automerge_1 = require("automerge");
 Object.defineProperty(exports, "Frontend", { enumerable: true, get: function () { return automerge_1.Frontend; } });
 Object.defineProperty(exports, "Text", { enumerable: true, get: function () { return automerge_1.Text; } });
 Object.defineProperty(exports, "change", { enumerable: true, get: function () { return automerge_1.change; } });
-var helpers_1 = require("cambria/dist/helpers");
-Object.defineProperty(exports, "inside", { enumerable: true, get: function () { return helpers_1.inside; } });
-Object.defineProperty(exports, "addProperty", { enumerable: true, get: function () { return helpers_1.addProperty; } });
-Object.defineProperty(exports, "plungeProperty", { enumerable: true, get: function () { return helpers_1.plungeProperty; } });
-Object.defineProperty(exports, "removeProperty", { enumerable: true, get: function () { return helpers_1.removeProperty; } });
-Object.defineProperty(exports, "hoistProperty", { enumerable: true, get: function () { return helpers_1.hoistProperty; } });
-const cambriamerge_3 = require("./cambriamerge");
+var cambria_1 = require("cambria");
+Object.defineProperty(exports, "inside", { enumerable: true, get: function () { return cambria_1.inside; } });
+Object.defineProperty(exports, "addProperty", { enumerable: true, get: function () { return cambria_1.addProperty; } });
+Object.defineProperty(exports, "plungeProperty", { enumerable: true, get: function () { return cambria_1.plungeProperty; } });
+Object.defineProperty(exports, "removeProperty", { enumerable: true, get: function () { return cambria_1.removeProperty; } });
+Object.defineProperty(exports, "hoistProperty", { enumerable: true, get: function () { return cambria_1.hoistProperty; } });
 exports.Backend = {
-    init: cambriamerge_3.init,
-    applyChanges: cambriamerge_3.applyChanges,
-    applyLocalChange: cambriamerge_3.applyLocalChange,
-    getChanges: cambriamerge_3.getChanges,
-    getPatch: cambriamerge_3.getPatch,
-    getChangesForActor: cambriamerge_3.getChangesForActor,
-    getMissingChanges: cambriamerge_3.getMissingChanges,
-    getMissingDeps: cambriamerge_3.getMissingDeps,
-    merge: cambriamerge_3.merge
+    init: cambriamerge_1.init,
+    applyChanges: cambriamerge_1.applyChanges,
+    applyLocalChange: cambriamerge_1.applyLocalChange,
+    getChanges: cambriamerge_1.getChanges,
+    getPatch: cambriamerge_1.getPatch,
+    getChangesForActor: cambriamerge_1.getChangesForActor,
+    getMissingChanges: cambriamerge_1.getMissingChanges,
+    getMissingDeps: cambriamerge_1.getMissingDeps,
+    merge: cambriamerge_1.merge,
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-export { init, applyChanges, applyLocalChange, getChanges, getPatch } from "./cambriamerge"
-export { RegisteredLens, CambriaBlock as Change, CambriaBackend as BackendState } from "./cambriamerge"
-export { Frontend, Doc, ChangeFn, Proxy, Clock, Change as Request, Patch, Text, change } from 'automerge'
-export { inside, addProperty, plungeProperty, removeProperty, hoistProperty } from 'cambria/dist/helpers'
+import {
+  init,
+  applyChanges,
+  applyLocalChange,
+  getChanges,
+  getPatch,
+  getChangesForActor,
+  getMissingChanges,
+  getMissingDeps,
+  merge,
+} from './cambriamerge'
 
-import { init, applyChanges, applyLocalChange, getChanges, getPatch, getChangesForActor, getMissingChanges, getMissingDeps, merge } from "./cambriamerge"
+export { init, applyChanges, applyLocalChange, getChanges, getPatch } from './cambriamerge'
+export {
+  RegisteredLens,
+  CambriaBlock as Change,
+  CambriaBackend as BackendState,
+} from './cambriamerge'
+export {
+  Frontend,
+  Doc,
+  ChangeFn,
+  Proxy,
+  Clock,
+  Change as Request,
+  Patch,
+  Text,
+  change,
+} from 'automerge'
+export { inside, addProperty, plungeProperty, removeProperty, hoistProperty } from 'cambria'
 
 export const Backend = {
   init,
@@ -14,6 +38,5 @@ export const Backend = {
   getChangesForActor,
   getMissingChanges,
   getMissingDeps,
-  merge
+  merge,
 }
-
